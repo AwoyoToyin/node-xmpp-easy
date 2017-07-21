23b06de10a90aa289e3ca322c745657f8ed449f4
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-07-21 06:54:42 +0100

    client connection is saved after authentication

--- event/custom/authenticate.js
+++ event/custom/authenticate.js
@@ -4,13 +4,26 @@
  * @param client
  * @return function(opt, cb)
  */
+const CLIENTS = require('../../util/clients')
+
 module.exports = client => (opts, cb) => {
     //do authentication / password check here
     var authenticated = true
 
-    if(authenticated){ //truthy
+    if (authenticated) { //truthy
         cb(null, opts)
-    }else{ //false
+        saveClient(client)
+    } else { //false
         cb(false)
     }
+
+    console.log("i'm authenticated")
+}
+
+function saveClient(client) {
+    console.log('server:', client.jid.local, 'ONLINE')
+    const clientKey = `${client.jid.user}@${client.jid.domain}`
+    client['clientKey'] = clientKey
+    //store client informaiton
+    CLIENTS.set(clientKey, client)
 }
