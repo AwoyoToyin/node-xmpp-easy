9acdc4e8db43bbd5a92e1a47cc0e52df9eaaa015
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-07-21 06:55:44 +0100

    added check for undefined

--- event/custom/disconnect.js
+++ event/custom/disconnect.js
@@ -2,8 +2,10 @@ const CLIENTS = require('../../util/clients')
 /**
  * This is called when a client disconnects from the server
  */
-module.exports = client => () =>{
-    console.log('server:', client.jid.local, 'DISCONNECT')
-    const clientKey = `${client.jid.user}@${client.jid.domain}`
-    CLIENTS.delete(clientKey)
+module.exports = client => () => {
+    if (!client.jid == undefined) {
+        console.log('server:', client.jid.local, 'DISCONNECT')
+        const clientKey = `${client.jid.user}@${client.jid.domain}`
+        CLIENTS.delete(clientKey)
+    }
 }
